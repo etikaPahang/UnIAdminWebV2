a300bb85fd4e791d9af57bf6f19b29cc01427eef
watef <lulukwatef76@gmail.com>
2017-12-03 14:36:08 +0700

    resolve events, monthly challanges

--- home.php
+++ home.php
@@ -23,10 +23,11 @@ body
     </a> 
     <a href="reports.php"><button type="button" class="btn btn-primary btn-lg" style="width : 25%;margin: 10%; margin-left: 15%; margin-right: 15%; margin-top: 8%" >Reports</button>
     </a>
-    <button type="button" class="btn btn-primary btn-lg" style="width : 25%;margin: 10%;  margin-left: 15%;  margin-right: 2%; margin-top: -5%"> Monthly Challange</button>
+    <a href="monthly.php"> <button type="button" class="btn btn-primary btn-lg" style="width : 25%;margin: 10%;  margin-left: 15%;  margin-right: 2%; margin-top: -5%"> Monthly Challange</button>
+    </a>
     <a href="complaint.php"><button type="button" class="btn btn-primary btn-lg" style="width : 25%;margin: 10%; margin-left: 15%; margin-right: 15%; margin-top: -5%  " >Complaint</button>
     </a>
-    <button type="button" class="btn btn-primary btn-lg" style="width : 50%;margin: 10%; margin-left: 25%; margin-right: 25% ; margin-top: -5%" >Add/Delete Students</button >
+  
     
 </body>
 </html>
